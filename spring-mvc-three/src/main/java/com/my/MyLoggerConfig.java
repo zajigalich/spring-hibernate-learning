f00e8cd0e289268b27a50b2cc5410e273a15adac
Konstantin <titarenko42k@gmail.com>
2023-02-06 01:44:28 +0200

    add Spring MVC configuration fixed

---
+++ spring-mvc-three/src/main/java/com/my/MyLoggerConfig.java
@@ -0,0 +1,54 @@
+package com.my;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+@Component
+public class MyLoggerConfig {
+
+    @Value("${root.logger}")
+    private String rootLoggerLevel;
+    @Value("${printed.logger}")
+    private String printedLoggerLevel;
+
+    public void setRootLoggerLevel(String rootLoggerLevel) {
+        this.rootLoggerLevel = rootLoggerLevel;
+    }
+
+    public void setPrintedLoggerLevel(String printedLoggerLevel) {
+        this.printedLoggerLevel = printedLoggerLevel;
+    }
+
+    @PostConstruct
+    public void initLogger() {
+
+        // parse levels
+        Level rootLevel = Level.parse(rootLoggerLevel);
+        Level printedLevel = Level.parse(printedLoggerLevel);
+
+        // get logger for app context
+        Logger applicationContextLogger = Logger.getLogger(AnnotationConfigApplicationContext.class.getName());
+
+        // get parent logger
+        Logger loggerParent = applicationContextLogger.getParent();
+
+        // set root logging level
+        loggerParent.setLevel(rootLevel);
+
+        // set up console handler
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(printedLevel);
+        consoleHandler.setFormatter(new SimpleFormatter());
+
+        // add handler to the logger
+        loggerParent.addHandler(consoleHandler);
+    }
+
+}
