a040f58ae1c5bc543697df4d78120d107a37152b
Konstantin <titarenko42k@gmail.com>
2023-02-11 02:17:57 +0200

    add Hibernate CRUD (create objects)

---
+++ hibernate-tutorial-four/src/main/java/com/my/demo/CreateStudentDemo.java
@@ -0,0 +1,40 @@
+package com.my.demo;
+
+import com.my.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure() //configure(hibernate.cfg.xml)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // truncate hb_student_tracker.student to recreate(reset) table
+
+        // alter table hb_student_tracker.student auto_increment = 1 to changing starting index
+
+        Student student1 = new Student("Bonita", "Stick", "bonita@gmail.com");
+        Student student2 = new Student("Garry", "Potter", "garry@gmail.com");
+        Student student3 = new Student("Tom", "Dick", "tom42k@gmail.com");
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            session.save(student1);
+            session.save(student2);
+            session.save(student3);
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+}
--- hibernate-tutorial-four/src/main/java/com/my/entity/Student.java
+++ hibernate-tutorial-four/src/main/java/com/my/entity/Student.java
@@ -2,6 +2,8 @@ package com.my.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +12,8 @@ import javax.persistence.Table;
 public class Student {
 
     @Id
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //used cause of feature in MySQl AUTO_INCREMENT
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(name = "first_name")
