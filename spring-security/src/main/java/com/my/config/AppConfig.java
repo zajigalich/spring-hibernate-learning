7f479a26e9f34b65f3d0a4ef56fbef3c8288e552
Konstantin <titarenko42k@gmail.com>
2023-02-23 01:58:40 +0200

    Spring Security Getting Started(add configuration no xml)

---
+++ spring-security/src/main/java/com/my/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.my.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.my")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
+
---
+++ spring-security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+
+<h2>Home Page</h2>
+<hr>
+
+Welcome to home page
+
+</body>
+</html>
