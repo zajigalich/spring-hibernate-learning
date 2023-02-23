03e248a88a6d5b0074ed52596c45dec28e483ba6
Konstantin <titarenko42k@gmail.com>
2023-02-23 03:12:31 +0200

    Spring Security Getting Started(spring security config)

---
+++ spring-security/src/main/java/com/my/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.my.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails garry = User.builder()
+                .username("garry")
+                .password("{noop}test123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails semen = User.builder()
+                .username("semen")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails dic = User.builder()
+                .username("dic")
+                .password("{noop}test123")
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(garry, semen, dic);
+    }
+
+    /*@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+            }
+        })
+                .formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+                    @Override
+                    public void customize(FormLoginConfigurer<HttpSecurity> httpSecurityFormLoginConfigurer) {
+                        httpSecurityFormLoginConfigurer
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll();
+                    }
+                })
+                .build();
+    }*/
+}
