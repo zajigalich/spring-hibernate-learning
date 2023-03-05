ca790b0058d1e375a3823e930a84cb64890f02f7
Konstantin <titarenko42k@gmail.com>
2023-03-05 23:52:27 +0200

    Spring Rest - Using PathVariable annotation for REST Endpoints

--- spring-rest-eight/src/main/java/com/my/rest/StudentRestController.java
+++ spring-rest-eight/src/main/java/com/my/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.my.rest;
 
 import com.my.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +14,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> students;
+
+    @PostConstruct
+    private void loadData(){
+        students = new ArrayList<>();
+        students.add(new Student("One", "First"));
+        students.add(new Student("Two", "Second"));
+        students.add(new Student("Tree", "Third"));
+    }
 
     //defining endpoint for "/students" - return list of students
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-
-        students.add(new Student("One", "First"));
-        students.add(new Student("Two", "Second"));
-        students.add(new Student("Tree", "Third"));
-
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
