36c2bfb5f1203f5a0938a834c6c513c5a31cdb6e
Konstantin <titarenko42k@gmail.com>
2023-03-12 17:22:18 +0200

    Rest Client App - add spring security(deprecated code)

---
+++ spring-crm-rest/src/main/java/com/my/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.my.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
